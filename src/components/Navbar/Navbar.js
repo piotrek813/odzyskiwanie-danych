6f2c33e9642f958820bf7be47bd7ac57619bd447
peters04 <piotreksmolinski04@gmail.com>
2020-03-26 16:51:57 +0100

    add theme file finish navbar and add temporary logo

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import posed from 'react-pose';
+import media, { sizes } from 'utils/media';
+import Hamburger from './Hamburger';
+
+const MenuWrapper = posed.div({
+  visible: {
+    applyAtStart: { display: 'flex' },
+    opacity: 0.9,
+  },
+  hidden: {
+    applyAtEnd: { display: 'none' },
+    opacity: 0,
+  },
+});
+
+const StyledWrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 13px;
+
+  ${media.tablet`
+    padding: 34px 50px;
+  `}
+`;
+
+const StyledHamburger = styled(Hamburger)`
+  z-index: 9999;
+
+  ${media.tablet`
+    display: none;
+  `};
+`;
+
+const StyledMenuWrapper = styled(MenuWrapper)`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: ${({ theme }) => theme.font.weight.medium};
+  background: ${({ theme }) => theme.dark};
+  color: ${({ theme }) => theme.light};
+
+  ${media.tablet`
+    position: static;
+    background: none;
+    width: auto;
+    height: auto;
+    color: ${({ theme }) => theme.dark};
+  `}
+`;
+
+const StyledList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  ${media.tablet`
+      flex-direction: row;
+  `}
+`;
+
+const StyledItem = styled.li`
+  text-transform: uppercase;
+  padding: 25px;
+  width: 100%;
+  text-align: center;
+
+  ${media.tablet`
+    padding: 0 30px;
+    padding-right: 0;
+    width: auto;
+  `}
+`;
+
+class Navbar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isMenuOpen: false,
+    };
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  handleMenuToggle = () => {
+    this.setState((prevState) => ({
+      isMenuOpen: !prevState.isMenuOpen,
+    }));
+  };
+
+  updateWindowDimensions = () => {
+    if (window.innerWidth >= sizes.tablet) {
+      this.setState({ isMenuOpen: true });
+    } else {
+      this.setState({ isMenuOpen: false });
+    }
+  };
+
+  render() {
+    const navItems = [
+      'HDD',
+      'SSD',
+      'RAID',
+      'Dyski przenośne',
+      'kontakt',
+      'blog',
+    ];
+    const { isMenuOpen, isTablet } = this.state;
+    return (
+      <StyledWrapper>
+        <p>logo</p>
+        <StyledHamburger onClick={this.handleMenuToggle} isOpen={isMenuOpen} />
+        <StyledMenuWrapper
+          isTablet={isTablet}
+          pose={isMenuOpen ? 'visible' : 'hidden'}
+        >
+          <StyledList>
+            {navItems.map((item) => (
+              <StyledItem key={item}>{item}</StyledItem>
+            ))}
+          </StyledList>
+        </StyledMenuWrapper>
+      </StyledWrapper>
+    );
+  }
+}
+
+export default Navbar;
