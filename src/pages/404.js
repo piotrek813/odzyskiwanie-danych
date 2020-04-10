421ebd37c63fefd870d8d3d5a2edbbc0b2e54c17
peters04 <piotreksmolinski04@gmail.com>
2020-04-10 18:18:30 +0200

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainTemplate from 'templates/MainTemplate';
+
+const PageNotFound = () => (
+  <MainTemplate
+    hero={{
+      heading: 'Strona nie istnieje',
+    }}
+  />
+);
+
+export default PageNotFound;
