11f56286faaae730d5266dcda9b9520d70887cc6
peters04 <piotreksmolinski04@gmail.com>
2020-04-19 17:37:42 +0200

    change color in manifest.json and propably fix center of map

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,11 +48,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Odzyskiwanie danych Warszawa Mokotów`,
+        short_name: `Odzyskiwanie Danych`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#222831`,
+        theme_color: `#222831`,
         display: `minimal-ui`,
         icon: `src/assets/images/logo_all_data_recovery.png`,
       },
