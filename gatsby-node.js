3f59d406d72f6fcf6fdd1039c193bfe5d561e325
peters04 <piotreksmolinski04@gmail.com>
2021-01-11 18:50:18 +0100

    create htaccess onPostBuild gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,7 @@
+// this exception for eslint is necessary because of htaccess file
+/* eslint-disable no-useless-escape */
 const path = require('path');
+const fs = require('fs');
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
@@ -45,4 +48,46 @@ exports.createPages = async ({ graphql, actions }) => {
       },
     });
   });
+};
+
+exports.onPostBuild = ({ store }) => {
+  const { program } = store.getState();
+
+  const htContent = `<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteCond %{HTTPS} off [OR]
+    RewriteCond %{HTTP_HOST} ^www\. [NC]
+    RewriteCond %{HTTP_HOST} ^(?:www\.)?(.+)$ [NC]
+    RewriteRule ^ https://%1%{REQUEST_URI} [L,NE,R=301]
+</IfModule>
+<IfModule mod_headers.c>
+    <FilesMatch "\.(html|json)$">
+        Header set Cache-Control "public, max-age=0, must-revalidate"
+    </FilesMatch>
+    <FilesMatch "\.(js|css)$">
+        Header set Cache-Control "public, max-age=31536000, immutable"
+    </FilesMatch>
+    <Files sw.js>
+        Header set Cache-Control "public, max-age=0, must-revalidate"
+    </Files>
+</IfModule>`;
+
+  const htStaticContent = `<IfModule mod_headers.c>
+  Header set Cache-Control "public, max-age=31536000, immutable"
+</IfModule>`;
+
+  fs.writeFileSync(
+    `${program.directory}/public/.htaccess`,
+    htContent,
+    (err) => {
+      if (err) throw err;
+    }
+  );
+  fs.writeFileSync(
+    `${program.directory}/public/static/.htaccess`,
+    htStaticContent,
+    (err) => {
+      if (err) throw err;
+    }
+  );
 };
